a8ba374d7666cfbf8773a5da6aa74ae54921cfdb
Cristian Tahay <renetahay@mesoamericana.edu.gt>
2021-10-02 20:18:09 -0600

    Revert "ASA" cambios sin bulma This reverts commit 8dacd30f16f5982e5d953d082751ac1c7bf4d5a9.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('publicaciones/<int:pk>', views.publicacion_detalle, name ='publicacion_detalle'),
     path('publicacion/nueva', views.publicacion_nueva, name ='publicacion_nueva'),
     path('publicacion/<int:pk>/editar', views.publicacion_editar, name ='publicacion_editar'),
+    path('borradores/', views.publicacion_borrador_lista, name ='publicacion_borrador_lista'),
+    path('publicacion/<pk>/publicar/', views.publicacion_publicar, name ='publicacion_publicar'),
+    path('publicacion/<pk>/borrar/', views.publicacion_borrar, name ='publicacion_borrar'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from . models import Publicacion
 from django.utils import timezone
 from . forms import PublicacionForm
-
+from django.contrib.auth.decorators import login_required
 
 def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter (fecha_publicacion__lte = timezone.now()).order_by('fecha_publicacion')
@@ -12,8 +12,9 @@ def publicacion_detalle(request,pk):
     publicacion = get_object_or_404(Publicacion, pk = pk)
     return render(request,'blog/publicacion_detalle.html', {'publicacion': publicacion})
 
+@login_required
 def publicacion_nueva(request):
-    if request.method =="POST":
+    if request.method == "POST":
         formulario = PublicacionForm(request.POST)
         if formulario.is_valid():
             publicacion = formulario.save(commit=False)
@@ -24,6 +25,7 @@ def publicacion_nueva(request):
         formulario = PublicacionForm()
     return render(request,'blog/publicacion_editar.html', {'formulario': formulario})
 
+@login_required
 def publicacion_editar(request,pk):
     publicacion = get_object_or_404(Publicacion, pk = pk)
     if request.method =="POST":
@@ -36,3 +38,20 @@ def publicacion_editar(request,pk):
     else:
         formulario = PublicacionForm(instance=publicacion)
     return render(request,'blog/publicacion_editar.html', {'formulario': formulario})
+
+@login_required
+def publicacion_borrador_lista(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_publicacion')
+    return render(request, 'blog/publicacion_borrador_lista.html', {'publicaciones': publicaciones})
+
+@login_required
+def publicacion_publicar(request,pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.publicar()
+    return redirect('publicacion_detalle', pk=pk)
+
+@login_required
+def publicacion_borrar(request,pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.delete()
+    return redirect('publicacion_lista')
